d3d2d4c178f5fa1f0a3dd6c858832f604df52958
Ephraim <ephraimibok@gmail.com>
2022-09-08 16:58:34 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
+
 /**
  *
- *  *main- Serves as the entry point for the program
+ * * main - Printing
  *
- *   *
+ * *
  *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ * * Return: Always 0 (Successful)
  *
- *     */
+ * */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual
+puzzle");
 
-			return (0);
+	return (0);
 
 }
-
