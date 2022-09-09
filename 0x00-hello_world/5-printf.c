8bcd11293a2796fa32117f1826b2e51e25c6bc19
Ephraim <ephraimibok@gmail.com>
2022-09-08 19:55:23 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- * main -entry pointfo for the program
+ * main - print the string in the put function
  *
- * Return:0 if no errors, return non zero if errors
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
