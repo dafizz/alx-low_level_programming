eb910e8a8b14de60bc07495620eb0eac90047e87
Ephraim <ephraimibok@gmail.com>
2022-10-06 03:30:33 +0100

    Update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: An array of size argc
  * Return: always successful
  */
-int main(int argc _attribute_((unused)), char *argv)
+int main(int argc __attribute__((unused)), char *argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
